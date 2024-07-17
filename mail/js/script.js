87111e753defaf125acc48bd3d9dc86221d50a75
ChristopherS24 <chryskies@gmail.com>
2024-07-17 03:02:37 +0200

    correzioni, e fine primo esercizio

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,23 +1,36 @@
-//nome utente
-const email = prompt('inserisci la tua email:');
-console.log ('email utente:' , email);
+//form
+const form = document.querySelector ('form');
 
-//array
-const autorizzati = ['qui' , 'quo' , 'qua'];
-console.log ('autorizzato', autorizzati);
+//evento
+form.addEventListener ('submit' , 
+    function(event) {
+        event.preventDefault();
 
-//condizione
-if (email == autorizzati [0]) {
-    console.log ('email autorizzata:', autorizzati[0]);
-}
-else if(email == autorizzati [1]) {
-    console.log ('email autorizzata:', autorizzati[1]);
-}
-else if(email == autorizzati [2]) {
-    console.log ('email autorizzata:', autorizzati[2]);
-}
-else {
-    console.log ('email NON autorizzata');
-    alert('email NON autorizzata')
-}
+        //prendi e salva valori
+        const emailInput = document.getElementById ('email');
+        const email = emailInput.value;
+        console.log ('email inserita:' , email , typeof email);
+
+        //array
+        const autorizzati = ['qui@hotmail.com' , 'quo@hotmail.com' , 'qua@hotmail.com'];
+        console.log ('autorizzato', autorizzati);
+
+        //condizione
+        if (email == autorizzati [0]) {
+            console.log ('email autorizzata:', autorizzati[0]);
+        }
+        else if(email == autorizzati [1]) {
+            console.log ('email autorizzata:', autorizzati[1]);
+        }
+        else if(email == autorizzati [2]) {
+            console.log ('email autorizzata:', autorizzati[2]);
+        }
+        else {
+            console.log ('email NON autorizzata');
+            alert('email NON autorizzata');
+
+        }
+
+    }
+)
 
